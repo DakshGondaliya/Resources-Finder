37387594fbe64acd0e35fe9d9139b0cbe0206fbc
DakshGondaliya <gondaliyadaksh@gmail.com>
2019-02-02 01:03:57 +0530

    update smooth scroll up

--- script/main.js
+++ script/main.js
@@ -1,15 +1,15 @@
-window.onscroll = function () {
-  scrollindicator();
-};
+// window.onscroll = function () {
+//   scrollindicator();
+// };
 
-function scrollindicator() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+// function scrollindicator() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height =
+//     document.documentElement.scrollHeight -
+//     document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
 
 function toggleDarkLight() {
   var body = document.getElementById("body");
